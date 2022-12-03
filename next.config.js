8a1b36c8a835957f2a40309b6b0d6033515ab161
debdevs <debdevs97@gmail.com>
2022-12-03 16:54:20 -0500

    revise export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
+
+// module.exports = {
+//   // Use the CDN in production and localhost for development.
+//   assetPrefix: isProd ? '/' : undefined,
+// }
+
 
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? '/' : undefined,
-}
+    async redirects() {
+      return [
+        {
+          source: '/_next/',
+          destination: '/:',
+          permanent: false,
+        },
+      ]
+    },
+  }
