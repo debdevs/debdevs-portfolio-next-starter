aab771f1670f2ba9882caa4435dc96fc71d2656d
debdevs <debdevs97@gmail.com>
2022-12-03 16:38:54 -0500

    change build command

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-// module.exports = {
-//   // Use the CDN in production and localhost for development.
-//   assetPrefix: isProd ? '/' : undefined,
-// }
-
-
 module.exports = {
-
-    distDir: "_next",
-    generateBuildId: async () => {
-      if (process.env.BUILD_ID) {
-        return process.env.BUILD_ID;
-      } else {
-        return `${new Date().getTime()}`;
-      }
-    },
-
-  }
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? '/' : undefined,
+}
