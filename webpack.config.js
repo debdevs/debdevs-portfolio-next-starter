f8396501311f66816df797979531eb6b1325a40c
debdevs <debdevs97@gmail.com>
2022-12-03 16:41:22 -0500

    update build command

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
  
-module.exports = {
-    context: path.join(__dirname, '.'),
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'static' }
-            ]
-        })
-    ]
-};
+// module.exports = {
+//     context: path.join(__dirname, '.'),
+//     plugins: [
+//         new CopyWebpackPlugin({
+//             patterns: [
+//                 { from: 'static' }
+//             ]
+//         })
+//     ]
+// };
